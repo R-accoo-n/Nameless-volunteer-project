c429ed023fb95b519d9b7d62641057897d35791b
Nazar Dyda <nazardyda16@gmail.com>
2023-03-18 23:52:55 +0200

    NAMELESS-31_6.7 add post fundraising, FIX status fundraising

--- src/main/java/com/nameless/volunteerproject/services/FundraisingService.java
+++ src/main/java/com/nameless/volunteerproject/services/FundraisingService.java
@@ -1,43 +1,36 @@
 package com.nameless.volunteerproject.services;
 
+import com.nameless.volunteerproject.dto.FundraisingDto;
 import com.nameless.volunteerproject.dto.RequestDto;
 import com.nameless.volunteerproject.enums.FundraisingType;
 import com.nameless.volunteerproject.models.Fundraising;
 import com.nameless.volunteerproject.models.FundraisingRequest;
 import com.nameless.volunteerproject.repositories.FundraisingRepository;
 import com.nameless.volunteerproject.repositories.FundraisingRequestRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
-
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class FundraisingService {
+
     private final FundraisingRepository fundraisingRepository;
     private final FundraisingRequestRepository fundraisingRequestRepository;
 
-    @Autowired
-    public FundraisingService(FundraisingRepository fundraisingRepository, FundraisingRequestRepository fundraisingRequestRepository) {
-        this.fundraisingRepository = fundraisingRepository;
-        this.fundraisingRequestRepository = fundraisingRequestRepository;
-    }
     public List<Fundraising> getActiveFundraisingsByType(FundraisingType type) {
         return fundraisingRepository.findByTypeAndIsActive(type, true);
     }
+
     public List<Fundraising> getCompletedFundraisings(UUID userId) {
         return fundraisingRepository.findByUserIdAndIsActiveFalse(userId);
     }
-    public Fundraising createFundraising(Fundraising fundraising) {
-        fundraising.setId(UUID.randomUUID());
-        fundraising.setActive(true);
-        return fundraisingRepository.save(fundraising);
-    }
 
-
-    public void saveRequest(RequestDto requestDto){
-        FundraisingRequest fundraisingRequest=new FundraisingRequest();
+    public void saveRequest(RequestDto requestDto) {
+        FundraisingRequest fundraisingRequest = new FundraisingRequest();
         fundraisingRequest.setRequestName(requestDto.getName());
         fundraisingRequest.setWhom(requestDto.getWhom());
         fundraisingRequest.setGreyZone(requestDto.isGreyZone());
@@ -45,4 +38,34 @@ public class FundraisingService {
         fundraisingRequestRepository.save(fundraisingRequest);
     }
 
+    @GetMapping("/statusFundraisings(")
+    public List<Fundraising> getStatusFundraisings() {
+        return fundraisingRepository.findByIsActive(true);
+    }
+
+    public List<Fundraising> getActiveFundraisings() {
+        return fundraisingRepository.findByIsActive(true);
+    }
+
+    public Fundraising createFundraising(FundraisingDto fundraisingDto) {
+        fundraisingDto.setId(UUID.randomUUID());
+        fundraisingDto.setActive(true);
+        Fundraising fundraising = mapFundraisingDtoToFundraising(fundraisingDto);
+        return fundraisingRepository.save(fundraising);
+    }
+
+    private Fundraising mapFundraisingDtoToFundraising(FundraisingDto fundraisingDto) {
+        Fundraising fundraising = new Fundraising();
+        fundraising.setId(fundraisingDto.getId());
+        fundraising.setUserId(fundraisingDto.getUserId());
+        fundraising.setCardNumber(fundraisingDto.getCardNumber());
+        fundraising.setDescription(fundraisingDto.getDescription());
+        fundraising.setFundraisingName(fundraisingDto.getFundraisingName());
+        fundraising.setActive(fundraisingDto.isActive());
+        fundraising.setSum(fundraisingDto.getSum());
+        fundraising.setType(fundraisingDto.getType());
+        fundraising.setWhom(fundraisingDto.getWhom());
+        fundraising.setSelected(fundraisingDto.isSelected());
+        return fundraising;
+    }
 }
