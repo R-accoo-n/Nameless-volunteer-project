980585cb55d920076bd9c5535517d2fd308b2a43
marta2245 <125898114+marta2245@users.noreply.github.com>
2023-03-18 20:38:32 +0200

    login

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,13 @@
+// Get HTML elements
+const emailInput = document.querySelector('input[type="text"]');
+const passwordInput = document.querySelector('input[type="password"]');
+const loginButton = document.querySelector('.end-login');
+
+// Add event listener to login button
+loginButton.addEventListener('click', () => {
+  // Get user input
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  
+  
+});
