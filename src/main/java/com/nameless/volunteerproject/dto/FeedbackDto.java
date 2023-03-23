787a496a7357cbb8ad1ec162149e76bb956a1f0f
Nazar Dyda <nazardyda16@gmail.com>
2023-03-23 21:22:58 +0200

    NAMELESS-60_8.2_feedback backend part done

---
+++ src/main/java/com/nameless/volunteerproject/dto/FeedbackDto.java
@@ -0,0 +1,25 @@
+package com.nameless.volunteerproject.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FeedbackDto {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
+    private int rate;
+    private String description;
+}
--- src/main/java/com/nameless/volunteerproject/security/WebSecurityConfig.java
+++ src/main/java/com/nameless/volunteerproject/security/WebSecurityConfig.java
@@ -21,7 +21,7 @@ class SecurityConfiguration {
 
         http.authorizeHttpRequests()
                 // URL matching for accessibility
-                .antMatchers("/", "/login", "/register", "/home", "/registration", "/register/save", "/save", "/request", "/request/save", "/fundraising", "/fundraising/save", "/statusFundraising").permitAll()
+                .antMatchers("/", "/login", "/register", "/home", "/registration", "/register/save", "/save", "/request", "/request/save", "/fundraising", "/fundraising/save", "/statusFundraising", "/feedback").permitAll()
                 .antMatchers("/css/**", "/js/**", "/bootstrap/**", "/static/**", "/mixins/**", "/utilities/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
